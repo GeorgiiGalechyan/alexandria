00248a63967f021d19f29673cb0804c9ba1417f7
Georgii Galechyan <galechyan1991@gmail.com>
2023-01-01 17:29:06 +0400

    случайно удалил скобку

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -19,7 +19,7 @@ const IndexPage = () => {
       </section>
     </Layout>
   )
-
+}
 // Метаданные - Gatsby Head API
 // https://www.gatsbyjs.com/docs/reference/built-in-components/gatsby-head/
 export const Head = () => <Seo title="Домашняя Страница" />
