e763bbcce81273a19e98db8841f8aa6a28c3104e
Georgii Galechyan <galechyan1991@gmail.com>
2023-02-14 22:02:33 +0400

    Пройдена 7 часть учебника Gatsby

--- src/pages/blog/index.jsx
+++ src/pages/blog/index.jsx
@@ -1,18 +1,21 @@
 import * as React from 'react'
-import { graphql } from 'gatsby'
+import { Link, graphql } from 'gatsby'
 import MainLayout from '../../Layouts/MainLayout/Layout'
 import Seo from '../../components/Seo/Seo.js'
 
-import {} from './index.module.css'
+import { postCard } from './index.module.css'
 
 const BlogPage = ({ data }) => {
   const posts = data.allMdx.nodes
+
   return (
     <MainLayout pageName="Главная">
       <section>
         {posts.map((post) => (
-          <article key={post.id} style={{ border: '1px solid var(--viva-magenta)', margin: '1.5em', padding: '1em' }}>
-            <h2>{post.frontmatter.title}</h2>
+          <article key={post.id} className={postCard}>
+            <h2>
+              <Link to={`/blog/${post.frontmatter.slug}`}>{post.frontmatter.title}</Link>
+            </h2>
             <p>Опубликовано: {post.frontmatter.date}</p>
             <div>{post.excerpt}</div>
           </article>
@@ -21,6 +24,7 @@ const BlogPage = ({ data }) => {
     </MainLayout>
   )
 }
+
 // Метаданные - Gatsby Head API
 // https://www.gatsbyjs.com/docs/reference/built-in-components/gatsby-head/
 export const Head = () => <Seo title="Блог" />
@@ -32,6 +36,7 @@ export const query = graphql`
         frontmatter {
           date(formatString: "MMMM D, YYYY")
           title
+          slug
         }
         id
         excerpt
--- src/pages/blog/{mdx.frontmatter__slug}.js
+++ src/pages/blog/{mdx.frontmatter__slug}.js
@@ -1,15 +1,28 @@
 import * as React from 'react'
+import { Link, graphql } from 'gatsby'
 import MainLayout from '../../Layouts/MainLayout/Layout'
 import Seo from '../../components/Seo/Seo.js'
 
-const BlogPost = () => {
+const BlogPost = ({ data, children }) => {
   return (
-    <MainLayout pageTitle="Super Cool Blog Posts">
-      <p>My blog post contents will go here (eventually).</p>
+    <MainLayout pageTitle={data.mdx.frontmatter.title}>
+      <p>{data.mdx.frontmatter.date}</p>
+      {children}
     </MainLayout>
   )
 }
 
-export const Head = () => <Seo title="Super Cool Blog Posts" />
+export const query = graphql`
+  query ($id: String) {
+    mdx(id: { eq: $id }) {
+      frontmatter {
+        title
+        date(formatString: "MMMM D, YYYY")
+      }
+    }
+  }
+`
+
+export const Head = ({ data }) => <Seo title={data.mdx.frontmatter.title} />
 
 export default BlogPost
